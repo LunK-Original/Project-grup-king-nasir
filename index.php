392c3e7bef1ae113aa9f7fdb0b19d52f4945b04c
Faris Maulana <faribz428@gmail.com>
2026-06-01 10:21:26 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php 
-$kalimat = "Selamat datang pendatang ..."
+session_start();
+
+if(isset($_SESSION["username"]))
+    {
+        header("Location: beranda.php");
+        exit();
+    }
+    else
+    {
+        header("Location: login.php");
+        exit();
+    }
+
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php include "layout/header.html"?>
-
-    <br>
-
-    <main>
-    <h1><?= $kalimat ?></h1>
-    </main>
-
-    <br>
-
-    <?php include "layout/footer.html"?>
-</body>
-</html>
---
+++ register.php
@@ -0,0 +1,58 @@
+<?php 
+
+include "layanan/database.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div>
+    <div class="register_text">
+        <h1>Register</h1>
+    </div>
+    <form action="" method="POST">
+        <input type="gmail" name="nama" id="" placeholder="Ketik nama anda" require>
+        <div></div>
+        <input type="password" name="password" id="" placeholder="Ketik password anda" require>
+        <?php 
+        
+$username = $_POST["nama"] ?? "";
+$password = $_POST["password"] ?? "";
+
+$db = mysqli_connect("localhost" ,"root" , "" , "mahasiswa");
+$sql = "INSERT INTO maha(GMAIL , PASS) VALUES('$username' , '$password')";
+
+if(isset($_POST["login"]))
+    {
+        if(empty($username) || empty($password))
+            {
+                echo "<br>Ada yang belum di isi";
+            }
+            else
+            {
+                if($db->query($sql))
+                    {
+                        echo "<br>data telah masuk";
+                        header("Location: login.php");
+                        exit();
+                    }
+                else
+                    {
+                        echo "<br>terjadi kesalahan";
+                       
+                    }
+            }
+    }
+        ?>
+        <p>Sudah punya akun? <a href="login.php">Masuk sekarang!</a></p>
+        <input type="submit" name="login" id="" placeholder="Ketik nama anda" require>
+    </form>
+    </div>
+</body>
+</html>
